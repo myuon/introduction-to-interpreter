f087c9be62697f5b3fa999180be70674defe654c
myuon <ioi.joi.koi.loi@gmail.com>
2024-02-28 23:23:37 +0900

    feat: parse +, -

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const numberLiteralExp = /^\-?\d?\.?\d+/;
+const numberLiteralExp = /^\-?\d+(\.\d+)?/;
 
 const consumeNumberLiteral = (t: string): string =>
   t.match(numberLiteralExp)?.[0] ?? "";
@@ -53,7 +53,7 @@ interface Token {
   number?: number;
 }
 
-const lexer = (input: string): Token[] => {
+const runLexer = (input: string): Token[] => {
   const tokens: Token[] = [];
   let position = 0;
   while (position < input.length) {
@@ -124,7 +124,144 @@ if (import.meta.vitest) {
 
   for (const test of tests) {
     it(`should return ${JSON.stringify(test.want)} for ${test.input}`, () => {
-      expect(lexer(test.input)).toEqual(test.want);
+      expect(runLexer(test.input)).toEqual(test.want);
+    });
+  }
+}
+
+type AST =
+  | {
+      type: "binaryOperator";
+      operator: "plus" | "minus" | "mult" | "div";
+      left: AST;
+      right: AST;
+    }
+  | {
+      type: "number";
+      value: number;
+    };
+
+const runParse = (tokens: Token[]): AST => {
+  let position = 0;
+
+  const term1 = (): AST => {
+    let current = number();
+
+    while (position < tokens.length) {
+      const token = tokens[position];
+      if (token.type === "plus" || token.type === "minus") {
+        position++;
+        current = {
+          type: "binaryOperator",
+          operator: token.type,
+          left: current,
+          right: number(),
+        };
+      } else {
+        break;
+      }
+    }
+
+    return current;
+  };
+  const number = (): AST => {
+    const token = tokens[position];
+    if (token.type === "number") {
+      position++;
+      return { type: "number", value: token.number! };
+    }
+
+    throw new Error("Expected number");
+  };
+
+  return term1();
+};
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+
+  const tests = [
+    {
+      input: "200.2",
+      want: {
+        type: "number",
+        value: 200.2,
+      },
+    },
+    {
+      input: "4 + 2 - 1",
+      want: {
+        type: "binaryOperator",
+        operator: "minus",
+        left: {
+          type: "binaryOperator",
+          operator: "plus",
+          left: { type: "number", value: 4 },
+          right: { type: "number", value: 2 },
+        },
+        right: { type: "number", value: 1 },
+      },
+    },
+    {
+      input: "1 + 2 + 3 + 4 + 5 + 6 + 7",
+      want: {
+        type: "binaryOperator",
+        operator: "plus",
+        left: {
+          type: "binaryOperator",
+          operator: "plus",
+          left: {
+            type: "binaryOperator",
+            operator: "plus",
+            left: {
+              type: "binaryOperator",
+              operator: "plus",
+              left: {
+                type: "binaryOperator",
+                operator: "plus",
+                left: {
+                  type: "binaryOperator",
+                  operator: "plus",
+                  left: {
+                    type: "number",
+                    value: 1,
+                  },
+                  right: {
+                    type: "number",
+                    value: 2,
+                  },
+                },
+                right: {
+                  type: "number",
+                  value: 3,
+                },
+              },
+              right: {
+                type: "number",
+                value: 4,
+              },
+            },
+            right: {
+              type: "number",
+              value: 5,
+            },
+          },
+          right: {
+            type: "number",
+            value: 6,
+          },
+        },
+        right: {
+          type: "number",
+          value: 7,
+        },
+      },
+    },
+  ];
+
+  for (const test of tests) {
+    it(`should return ${JSON.stringify(test.want)} for ${test.input}`, () => {
+      expect(runParse(runLexer(test.input))).toEqual(test.want);
     });
   }
 }
